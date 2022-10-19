0a42cf9f3bf741bac47dbc2e395bbc6dcc4a0999
shoeJuice <remsfield.papillon@student.fairfield.edu>
2022-10-18 20:24:44 -0400

    Adding middleware

--- pages/api/authors.ts
+++ pages/api/authors.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import type { NextRequest } from "next/server";
-import Cors from "cors";
+
 import {
   addDoc,
   query,
@@ -11,47 +11,12 @@ import {
   getDocs,
 } from "firebase/firestore";
 import { firestore, getFirebaseAdmin } from "../../config/firebaseConfig";
-import rateLimit from "express-rate-limit";
 
-const cors = Cors({
-  methods: ["GET", "POST"],
-  origin: "http://localhost:3000",
-});
-
-const requestIP = (req : any) => {
-  const userIP = req.ip || req.headers["x-forwarded-for"] || req.headers["x-real-ip"];
-  console.log(userIP);
-  return userIP;
-};
-
-function runMiddleware(
-  req: NextApiRequest,
-  res: NextApiResponse,
-  fn: Function
-) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result: any) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-      console.log("Running");
-      let clientIP = requestIP(req);
-      rateLimit({
-        keyGenerator: (req, res) => req.ip,
-        windowMs: 100,
-        max: 5,
-        handler: (req, res) => res.status(429).send("Rate Limiter working"),
-      });
-      return resolve(result);
-    });
-  });
-}
 
 export default async function fetchAllAuthors(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  await runMiddleware(req, res, cors);
   const author_list = collection(firestore, "author_list");
   const allAuthors = query(author_list);
   const { method } = req;
